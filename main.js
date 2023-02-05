5f47f70b00ad0a8537a51ea4051d3fc1a2e4cead
jethro-ronald-lee <99052024+jethro-ronald-lee@users.noreply.github.com>
2023-02-05 00:44:17 -0500

    fixed button formatting and enforced a spell check

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ let fibonacci_first = 0;
 let fibonacci_second = 1;
 
 // to update the picture shown
-let eeveeList = ["https://www.jeumobi.com/wp-content/uploads/2021/08/eevee-evolutions-pokemon-go.jpg", 
+let eeveeList = ["img/eeveelutions.jpg", 
 	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/196.png", 
-	"https://img.pokemondb.net/artwork/large/sylveon.jpg", 
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/700.png",
 	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png",
 	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png",
 	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/136.png",
-	"https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_2.625/c_scale,w_400/ncom/en_US/games/switch/m/my-friend-peppa-pig-switch/description-image"];
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png",
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/470.png",
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/471.png",
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png",
+	"https://stock.flashmode.tn/wp-content/uploads/2020/06/peppa-pig-transparent-background-png-606.png"];
 
 // updates the value for the number of times the button was clicked and the image
 function buttonClicked() {
