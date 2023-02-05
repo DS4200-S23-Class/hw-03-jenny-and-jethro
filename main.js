442af34eb7745f9673834b55a1460ad79f9b1dea
jennycai01 <cai.je@northeastern.edu>
2023-02-04 19:00:17 -0500

    updates to button

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 // button function
 let fibonacci_first = 0;
 let fibonacci_second = 1;
+let eeveeList = ["https://www.jeumobi.com/wp-content/uploads/2021/08/eevee-evolutions-pokemon-go.jpg", 
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/196.png", 
+	"https://img.pokemondb.net/artwork/large/sylveon.jpg", 
+	"https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png"];
 
 // updates the value for the number of times the button was clicked
 function buttonClicked() {
@@ -18,4 +22,14 @@ function buttonClicked() {
 	let buttonDiv = document.getElementById("button-div");
 
 	buttonDiv.innerHTML = buttonClickTracker;
+
+
+	// changing button image
+	let img = new Image();
+	img.src = eeveeList[Math.floor(Math.random() * eeveeList.length)];
+	document.getElementById("buttonImg").src = img.src;
+
 }
+
+
+
